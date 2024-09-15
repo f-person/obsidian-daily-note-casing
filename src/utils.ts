b609e22ed8a9c899004364e9140628ebb1760cb8
Arshak Aghakaryan <agakaryan.arshak@gmail.com>
2024-09-15 16:40:43 +0200

    fix: use the plugin prefix in reportPluginError

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,6 @@ import { Notice } from "obsidian";
 export function reportPluginError(message: string): void {
 	const errorMessage = `[daily note casing]: ${message}`;
 
-	console.error(message);
-	new Notice(message);
+	console.error(errorMessage);
+	new Notice(errorMessage);
 }
